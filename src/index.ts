f9ebce51737d165c48cabcf9cc1df422321e31f1
Александр Алимов <a1im@mail.ru>
2022-11-28 17:41:41 +0400

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export * from './remoteQueue/types';
 
 export { JAWorker } from './worker/JAWorker';
 export { JARedisRemoteQueue } from './remoteQueue/JARedisRemoteQueue';
+export { createWaitExecutionJobs } from './helpers/createWaitExecutionJobs';
+export { createJAJob } from './helpers/createJAJob';
+export { generateStorageKey } from './helpers/generateStorageKey';
